6dfea12a0b70bbc61bd6ba8f1956680c01d091f9
maruma <maruyama_001@jimusuru.info>
2025-06-09 13:34:16 +0900

    Simplify server startup log

--- server.js
+++ server.js
@@ -65,5 +65,5 @@ app.delete('/customers/:id', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
